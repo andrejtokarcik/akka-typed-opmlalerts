1bbacd2ad435ec8746760fd00a89a58b85aebf9c
Andrej Tokarčík <andrejtokarcik@gmail.com>
2018-01-17 14:33:07 +0000

    Improved FeedHandler.fetcher: filtering newly added entries
    (Should be part of a separate behavior?)

--- src/main/scala/OpmlAlerts.scala
+++ src/main/scala/OpmlAlerts.scala
@@ -2,40 +2,60 @@ package opmlalerts
 
 import akka.actor.typed._
 import akka.actor.typed.scaladsl.Actor
-import akka.actor.typed.scaladsl.AskPattern._
-import akka.util.Timeout
 import java.net.URL
-import com.rometools.rome.io.SyndFeedInput
-//import java.util.Calendar
-//import java.util.Date
-//import scala.util.{Try, Success, Failure}
-
-import scala.concurrent.Future
-import scala.concurrent.duration._
-
+import com.rometools.rome.feed.synd.SyndEntry
+import com.rometools.rome.io.{ SyndFeedInput, XmlReader }
+import scala.collection.JavaConverters._
+import scala.util.{ Try, Success, Failure }
+import java.time.Instant
 
 object FeedHandler {
   final case class Fetch(replyTo: ActorRef[Download])
-  final case class Download(urls: List[URL])
+  final case class Download(url: URL)
 
-  val sfi = new SyndFeedInput
+  lazy val sfi = new SyndFeedInput
+  def parseFeed(feed: URL) = Try { sfi build new XmlReader(feed) }
+  def extractDate(x: SyndEntry): Option[Instant] = {
+    val date = Option(x.getUpdatedDate) orElse Option(x.getPublishedDate)
+    date map { _.toInstant }
+  }
 
   //override def preStart(): Unit = log.info("Feed handler of {} started", feed)
   //override def postStop(): Unit = log.info("Feed handler of {} stopped", feed)
 
-  def fetcher(feed: URL): Behavior[Fetch] =
+  def fetcher(feed: URL, lastFetched: Instant): Behavior[Fetch] =
     Actor.immutable[Fetch] { (ctx, msg) ⇒
-      ctx.system.log.info("Fetching {}...", feed)
-      msg.replyTo ! Download(List())
-      Actor.same
+      println(ctx.system.log.getClass)
+      ctx.system.log.info("Fetching and parsing feed '{}' for latest updates...", feed)
+      val maybeParsed = parseFeed(feed)
+      maybeParsed match {
+        case Success(v) ⇒
+          val newEntries = v.getEntries.asScala withFilter (extractDate(_) exists { _ isAfter lastFetched })
+          for (entry ← newEntries)
+            msg.replyTo ! Download(new URL(entry.getLink))
+
+        case Failure(e) ⇒
+          ctx.system.log.info("An exception occurred while processing of feed '{}': {}",
+                              feed, e.getMessage)
+      }
+      Actor.same  // TODO refresh lastFetched with Instant.now  + test
     }
 }
+
+
+//object Main extends App
+
+import akka.actor.typed.scaladsl.AskPattern._
+import akka.util.Timeout
+import scala.concurrent.Future
+import scala.concurrent.duration._
 
 object Main extends App {
   import FeedHandler._
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  val system: ActorSystem[Fetch] = ActorSystem(fetcher(new URL("http://localhost")), "fetcher")
+  val fetcherBeh = fetcher(new URL("http://lorem-rss.herokuapp.com/feed"), Instant.now)
+  val system: ActorSystem[Fetch] = ActorSystem(fetcherBeh, "fetcher")
 
   implicit val timeout = Timeout(5.seconds)
   implicit val sched = system.scheduler
